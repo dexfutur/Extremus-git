227fa425744733f97bb27089fbbf9c174063b382
Iuma <iumatoroyagari@gmail.com>
2022-03-01 08:48:23 -0500

    cursor

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Personaje/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController),typeof(playermove),typeof(playerrotate))]
+public class player : MonoBehaviour
+{
+    private playermove _move;
+    private playerrotate _rotate;
+
+    private void Awake()
+    {
+        _move = GetComponent<playermove>();
+        _rotate = GetComponent<playerrotate>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+        _move.Move();
+        _rotate.Rotate();
+    }
+}
---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Personaje/playermove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private CharacterController _player;
+    private Vector3 _moveDir;
+
+    private void Awake()
+    {
+        _player = GetComponent<CharacterController>();
+    }
+    public void Move()
+    {
+        _moveDir = ((transform.right * Input.GetAxis("Horizontal"))+(transform.forward*Input.GetAxis("Vertical")))*_speed;
+        _player.Move(_moveDir * Time.deltaTime);
+    }
+}
