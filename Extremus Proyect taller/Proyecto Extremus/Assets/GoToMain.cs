85ecc7056210b03389a83c92ac0970fa3f2bf35f
Iuma <iumatoroyagari@gmail.com>
2022-03-01 10:37:25 -0500

    Exit Volcano

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/GoToMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoToMain : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Here it is");
+        if (other.tag == "player")
+        {
+            SceneManager.LoadScene("Islas");
+        }
+
+    }
+    
+}
