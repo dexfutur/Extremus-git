d32ec6e80c8e3e4a3d79bb91827fc4f0b0344493
danielol312 <78271540+danielol312@users.noreply.github.com>
2022-03-16 13:18:13 -0500

    Transiciones(en proceso)

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Escenas/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float timetransition=2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+       
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(Loadlevel(SceneManager.GetActiveScene().buildIndex + 1));
+        
+    }
+
+    IEnumerator Loadlevel(int levelIndex)
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(timetransition);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
