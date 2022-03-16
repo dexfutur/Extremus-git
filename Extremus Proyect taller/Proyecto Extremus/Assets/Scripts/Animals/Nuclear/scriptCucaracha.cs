b2c83ab65587688ebdc15e24eb8a905a5cf6d461
Valeria Gonzalez Hernandez <79373892+ValeriaKaminari@users.noreply.github.com>
2022-03-15 23:34:59 -0500

    scripts avispa y cucaracha

--- Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Nuclear/scriptCucaracha.cs
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Nuclear/scriptCucaracha.cs
@@ -7,16 +7,17 @@ public class scriptCucaracha : MonoBehaviour
     public Camera camera;
     public ActiveNarrNuclear nar;
     public AudioNucl audios;
+    Animator animator;
   
 
 
     void Start()
     {
-      
+        animator = gameObject.GetComponent<Animator>();
     }
     private void RoachNarration()
     {
-        Debug.Log("PUMA");
+        Debug.Log("cucaracha");
         audios.PlayC();
     }
     void Update()
@@ -42,8 +43,9 @@ public class scriptCucaracha : MonoBehaviour
                 if (hitInfo.collider.gameObject.GetComponent<TargetC>() != null)
                 {
                     Debug.Log("la cucaracah camina");
-                   
-                   RoachNarration();
+
+                    animator.SetBool("semueve", true);
+                    RoachNarration();
                 }
 
             }
