66ee2e8f1cfa8047090a6ab5c2daab00b08462a7
danielol312 <78271540+danielol312@users.noreply.github.com>
2022-03-23 15:56:19 -0500

    pausa de nuevo y conteo para el final

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Escenas/ConteoEscenasFinal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConteoEscenasFinal : MonoBehaviour
+{
+   
+    static List <string> allScenes= new List<string> { "Volcánico", "Nuclear", "Marino" };
+    
+
+    private void Start()
+    {
+
+        if(allScenes.Count > 0)
+        {
+            var scene=allScenes[allScenes.Count-1];
+            SceneManager.LoadScene(scene);
+            allScenes.Remove(scene);
+        }
+        else
+        {
+            SceneManager.LoadScene("Despedida");
+        }
+    }
+}
--- Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/MenuPausa.cs
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/MenuPausa.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject botonPausa;
-    [SerializeField] private GameObject menupausa;     
+    [SerializeField] private GameObject menupausa;
+    private bool juegoPausado=false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+                else
+                {
+                    Pausa();
+                }
+            
+        }
+    }
     public void Pausa()
     {
+        juegoPausado = true;
         Time.timeScale = 0f;
         botonPausa.SetActive(false);
         menupausa.SetActive(true);
@@ -16,12 +34,18 @@ public class MenuPausa : MonoBehaviour
     }
     public void Reanudar()
     {
+        juegoPausado = false;
         Time.timeScale = 1f;
         botonPausa.SetActive(true);
         menupausa.SetActive(false);
     }
+    public void Configuracion()
+    {
+        juegoPausado=false;
+        SceneManager.LoadScene("Configuración");
+    }
     public void cerrar()
     {
-        Application.Quit();
+        Application.Quit(); 
     }
 }
