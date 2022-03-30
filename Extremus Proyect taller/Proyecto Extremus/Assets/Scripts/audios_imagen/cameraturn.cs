c97c0aaa9de5626e4629bd61a18a9ed8c6ecf7ea
Andres Felipe Perez Narvaez <91978851+Thezicksss@users.noreply.github.com>
2022-03-30 05:11:31 -0500

    camara isla volcanica

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/audios_imagen/cameraturn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraturn : MonoBehaviour
+{
+    public Transform target;
+    public float speed=1f, duration=19.064f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ApagarCamara", duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void ApagarCamara()
+    {
+        gameObject.SetActive(false);
+    }
+}
