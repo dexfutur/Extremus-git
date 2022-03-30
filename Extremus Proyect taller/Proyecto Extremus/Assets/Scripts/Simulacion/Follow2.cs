fba2995d77a542e0f390578d01ed0605ce9cd2ab
danielol312 <78271540+danielol312@users.noreply.github.com>
2022-03-30 16:57:34 -0500

    follow

---
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Simulacion/Follow2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow2 : MonoBehaviour
+{
+
+    Vector3 acceleration;
+    Vector3 displacement;
+
+    [SerializeField]
+    [Range(0, 10)]
+    float rapidez;
+
+    Vector3 velocity;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+
+    }
+    private Vector4 GetWorldMousePosition()
+    {
+        Camera camera = Camera.main;
+        Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+        worldPos.z = 0;
+        return worldPos;
+    }
+    public void Move()
+    {
+        Vector3 mousePos = GetWorldMousePosition();
+        Vector3 diferecia = mousePos - transform.position;
+        float radians = Mathf.Atan2(velocity.y, velocity.x);
+
+
+        acceleration = diferecia;
+        velocity += acceleration * Time.deltaTime;
+        displacement = velocity * Time.deltaTime;
+        transform.position += displacement;
+        transform.localRotation = Quaternion.Euler(0, 0, radians * Mathf.Rad2Deg);
+    }
+}
