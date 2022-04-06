6c72669cd245bd6af1201d543ca655af3c50fa6f
Andres Felipe Perez Narvaez <91978851+Thezicksss@users.noreply.github.com>
2022-04-06 12:46:09 -0500

    Programacion imagen

--- Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Marino/scriptCangrejo.cs
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Marino/scriptCangrejo.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scriptCangrejo : MonoBehaviour
 {
     public Camera camera;
     public ActiveNarrMarino nar;
     public AudioMarino audios;
+
+    //Imagen En Pantalla
+    public Image slot;
+    public Sprite img;
+    [SerializeField]
+    float tiempoDesaparicion = 10f;
+
     Animator animator;
 
     void Start()
@@ -31,9 +39,10 @@ public class scriptCangrejo : MonoBehaviour
 
     }
 
+    //Funcion al hacer click al animal
     public void ClickAction()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !animator.GetBool("semueve"))
         {
             //Ray goes through camera to position in the world the mouse points
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
@@ -44,15 +53,27 @@ public class scriptCangrejo : MonoBehaviour
                     Debug.Log("el cangrejo camina");
                     animator.SetBool("semueve", true);
                     CrabNarration();
+
+                    //Se asigna la imagen del animal y se pone el alpha en su maximo
+                    slot.sprite = img;
+                    Color clr = slot.color;
+                    clr.a = 255f;
+                    slot.color = clr;
+                    //Funcion para limpiar el slot de la imagen despues de [tiempoDesaparicion] segudos
+                    Invoke("ClearImage", tiempoDesaparicion);
                 }
                 
             }
         }
-
-        //Invoke("StopAnimation", 20);
     }
-
             
-    
+    void ClearImage()
+    {
+        //Quita la referencia a la imagen y pone el aplha en su minimo
+        slot.sprite =  null;
+        Color clr = slot.color;
+        clr.a = 0f;
+        slot.color = clr;
+    }
     
 }
